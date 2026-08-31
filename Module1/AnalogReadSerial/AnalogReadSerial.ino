6b6bae4cc77b0fd839764603d402a00f1b0ee53b
benkadener <benjamink@brandeis.edu>
2026-08-31 10:16:22 -0400

    added serial monitor

--- Module1/AnalogReadSerial/AnalogReadSerial.ino
+++ Module1/AnalogReadSerial/AnalogReadSerial.ino
@@ -13,7 +13,7 @@
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  Serial.begin(4800);
 }
 
 // the loop routine runs over and over again forever:
