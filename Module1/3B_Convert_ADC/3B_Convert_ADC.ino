d252c21e1f37dfdc483aa3b647aaf0b2dd4c4bc1
benkadener <benjamink@brandeis.edu>
2026-08-31 11:32:19 -0400

    added averaged and unavarged values

--- Module1/3B_Convert_ADC/3B_Convert_ADC.ino
+++ Module1/3B_Convert_ADC/3B_Convert_ADC.ino
@@ -6,12 +6,31 @@ void setup() {
 }
 
 void loop() {
+  int temp;
+  float totalVolt=0;
   int count = analogRead(analogPin);
   float voltage = count * Vref / 1023.0;
+  //100 sequential unaveraged voltage values
+  for (int i=0; i<100; i++) {
+    Serial.print("Unaveraged Voltage: ");
+    Serial.println(voltage, 4);
+  }
+  //100 averaged voltage values
+  for (int i=0; i<100; i++) {
+    for(int j=0; j<1000;j++){
+      totalVolt += voltage;
+    }
+    Serial.print("Averaged Voltage: ");
+    Serial.println(totalVolt/1000, 4);
+    float voltAve = totalVolt/1000;
+    totalVolt = 0;
 
-  Serial.print(count);
-  Serial.print(",");
-  Serial.println(voltage, 4);
+  }
+
+
+  // Serial.print(count);
+  // Serial.print(",");
+  // Serial.println(voltage, 4);
 
   delay(100);
 }
